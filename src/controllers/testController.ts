813cab95dd9acc5507a7f7d90cf7582d7e3cddaf
James Fan <jamesfan001@hotmail.com>
2025-05-06 14:03:57 -0400

    new cluster ...kszti3p

--- src/controllers/testController.ts
+++ src/controllers/testController.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from "express";
 
 export const getTestMessage = (req: Request, res: Response) => {
-  res.status(200).json({ message: `API/CONTROLER: Testing!!!` });
+  res.status(200).json({ message: `API/CONTROLER: Testing1!!!` });
 };
 
 export const getTest2Message = (req: Request, res: Response) => {
