b0335a0ff04fe232840cc1dcb6869c73d59b6ea8
James Fan <jamesfan001@hotmail.com>
2025-05-07 09:26:54 -0400

    20250507

--- src/middleware/authMiddleware.ts
+++ src/middleware/authMiddleware.ts
@@ -3,12 +3,8 @@ import asyncHandler from 'express-async-handler';
 import { Request, Response, NextFunction } from 'express';
 import User from '../models/userModel';
 
-interface DecodedToken {
-  id: string;
-}
-
 interface AuthenticatedRequest extends Request {
-  user?: Record<string, unknown>;
+  user?: any;
 }
 
 const protect = asyncHandler(async (req: AuthenticatedRequest, res: Response, next: NextFunction) => {
@@ -23,14 +19,14 @@ const protect = asyncHandler(async (req: AuthenticatedRequest, res: Response, ne
       token = req.headers.authorization.split(' ')[1];
 
       // Verify token
-      const decoded = jwt.verify(token, process.env.JWT_SECRET as string) as DecodedToken;
+      const decoded = jwt.verify(token, process.env.JWT_SECRET as string) as { id: string };
 
       // Get user from the token
       req.user = await User.findById(decoded.id).select('-password');
 
       next();
     } catch (error) {
-      console.error(error);
+      console.log(error);
       res.status(401);
       throw new Error('Not authorized');
     }
