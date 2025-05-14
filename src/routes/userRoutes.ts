592eee6c9c7a715508d09fe07f7749c4652d4c03
James Fan <jamesfan001@hotmail.com>
2025-05-14 12:15:49 -0400

    Version 1

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -4,17 +4,11 @@ import { protect } from '../middleware/authMiddleware';
 
 const router = express.Router();
 
-// router.post('/register',(req,res)=> {
-//   res.status(200).json({ message: 'User registered successfully' });
-// });
-router.post('/register', registerUser);
-// registerUser);
-router.post('/login', loginUser);
-// router.post('/login', (req, res) => {
-//   res.status(200).json({ message: 'Login successful' });
-// });
-// router.get('/me', (req, res) => {  res.status(200).json({ message: 'User data retrieved successfully' });});
-router.get('/profile/:id',getProfile);
-router.put('/update', updateUser)
-router.delete('/delete', deleteUser);
+
+router.post('/register', registerUser)
+router.post('/login', loginUser)
+router.get('/profile/:id',getProfile)
+router.put('/update',protect, updateUser)
+router.delete('/delete',protect, deleteUser)
+
 export default router;
