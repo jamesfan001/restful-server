55b623154650a6ff61fec0774e2415f6337c9c3c
James Fan <jamesfan001@hotmail.com>
2025-05-19 09:32:38 -0400

    change mode production

--- src/middleware/errorMiddleware.ts
+++ src/middleware/errorMiddleware.ts
@@ -15,7 +15,7 @@ const errorHandler = (err: Error, req: Request, res: Response, next: NextFunctio
 
   res.json({
     message: err.message ,
-    stack: process.env.NODE_ENV === 'productionx' ? null : err.stack,
+    stack: process.env.NODE_ENV === 'production' ? null : err.stack,
   });
 };
 
