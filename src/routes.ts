dac09b84dd59822b3d9c20d1692f35ff362391f9
James Fan <jamesfan001@hotmail.com>
2025-12-09 15:44:44 -0500

    commit

--- src/routes.ts
+++ src/routes.ts
@@ -7,7 +7,7 @@ const router = Router();
 router.use("/users", userRoutes); // Use the user routes for all requests to /api/users{
 
 router.get("/test", (req, res) => {
-  res.status(200).json({ message: "A PI SERVER IS STILL WORKING!!!!" });
+  res.status(200).json({ message: "API SERVER IS STILL WORKING!!!!" });
   // res.status(200).json({ message: "API server is still working!!!!" })
 });
 
