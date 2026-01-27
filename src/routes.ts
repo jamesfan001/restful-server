9937af1a760f02ed151b0198e39324fcec727b8b
James Fan <jamesfan001@hotmail.com>
2026-01-27 16:02:35 -0500

    01262026

--- src/routes.ts
+++ src/routes.ts
@@ -7,7 +7,7 @@ const router = Router();
 router.use("/users", userRoutes); // Use the user routes for all requests to /api/users{
 
 router.get("/test", (req, res) => {
-  res.status(200).json({ message: "**** O N L I N E ****" });
+  res.status(200).json({ message: "** O N L I N E " +process.cwd() + " **" });
   // res.status(200).json({ message: "API server is still working!!!!" })
 });
 
